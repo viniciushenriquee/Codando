7e956d8bc4e027d515ccd6fb9fb5a65c484d6a1c
vinicius henrique <viniciushenrique02@hotmail.com>
2025-03-13 16:18:15 -0300

    Retorno

---
+++ java/curso-udemy/Vetores/Altura/src/App.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantas pessoas serao digitadas: ");
+        int n = sc.nextInt();
+
+        String[] nomes = new String[n];
+        int [] idades = new int[n];
+        double[] altura = new double[n];
+
+        for(int i = 0; i < n; i++){
+            System.out.println("Dodas da " + (i+1) + "a pessoa: ");
+            System.out.print("nome: ");
+            nomes[i] = sc.next();
+            System.out.print("idade: ");
+            idades[i] = sc.nextInt();
+            System.out.print("altura: ");
+            altura[i] = sc.nextDouble();
+        }
+        double soma = 0.0;
+        for(int i = 0; i < n; i++){
+            soma = soma + altura[i];
+        }
+         double mediaAltura = soma / n;
+        
+        
+        System.out.printf("Altura media:  %.2f%n", mediaAltura);
+
+        int cont = 0;
+        for(int i = 0; i < n; i++){
+            if (idades[i] < 16) {
+                cont = cont + 1;
+            }
+        }
+
+        double porcent = cont * 100 / n;
+        System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n", porcent);
+
+        sc.close();
+    }
+
+}
--- java/curso-udemy/Vetores/Negativos/src/App.java
+++ java/curso-udemy/Vetores/Negativos/src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos numeros voce vai digitar?");
+        int n = sc.nextInt();
+
+        int[] vet = new int[n];
+
+         System.out.println("Digite os numeros: ");
+            for (int i = 0; i < n; i++){
+            vet[i] = sc.nextInt();
+        }
+
+        System.out.println("Negativos: ");
+        for(int i = 0; i < n; i++){
+            if(vet[i] < 0 ){
+                System.out.println(vet[i]);
+            }
+        }
+        sc.close();
     }
 }
